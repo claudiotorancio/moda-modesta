a8d63515863d703872be1c778bfd8cd3c7648bd6
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-09-01 15:23:24 -0300

    creando fcion hashContrellers 4

--- frontend/app.js
+++ frontend/app.js
@@ -19,35 +19,14 @@ import productoServices from "./services/product_services.js";
 import { modalControllers } from "./modal/modal.js";
 import { cargarReseñas } from "./controllers/productos/reseñas.js";
 import { Compras } from "./controllers/compras/compras-controllers.js";
-
+import { hashControllers } from "./controllers/hashControllers.js";
 // Función principal que se ejecuta cuando el DOM está listo
 document.addEventListener("DOMContentLoaded", async () => {
   const hash = window.location.hash;
   if (hash.startsWith("#product-")) {
     const id = hash.replace("#product-", "");
 
-    try {
-      // Encapsular el producto en un array si es un solo objeto
-      const response = await productoServices.detalleProducto(id);
-      const producto = response.product; // Asumiendo que el objeto está dentro de 'product'
-
-      // Convertir en array si es necesario
-      const productosArray = [producto];
-
-      productosArray.forEach((p) => {
-        mostrarProducto(
-          p.name,
-          p.price,
-          p.imagePath,
-          p.sizes,
-          p.description,
-          p._id
-        );
-      });
-    } catch (error) {
-      console.error("Error al obtener los detalles del producto:", error);
-    }
-  }
+   hashControllers()
   cargarReseñas();
   // modalControllers.modalSuscribe();
   // Obtener usuario autenticado de la sesión
---
+++ frontend/controllers/hashControllers.js
@@ -0,0 +1,25 @@
+import productoServices from "../services/product_services.js";
+
+export async function hashControllers() {
+  try {
+    // Encapsular el producto en un array si es un solo objeto
+    const response = await productoServices.detalleProducto(id);
+    const producto = response.product; // Asumiendo que el objeto está dentro de 'product'
+
+    // Convertir en array si es necesario
+    const productosArray = [producto];
+
+    productosArray.forEach((p) => {
+      mostrarProducto(
+        p.name,
+        p.price,
+        p.imagePath,
+        p.sizes,
+        p.description,
+        p._id
+      );
+    });
+  } catch (error) {
+    console.error("Error al obtener los detalles del producto:", error);
+  }
+}
