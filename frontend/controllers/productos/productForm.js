58fad68a9f3dae52ff4e77b3706b452808f83990
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-09-03 01:53:35 -0300

    mejorando visual modal movil 23

--- frontend/controllers/productos/productForm.js
+++ frontend/controllers/productos/productForm.js
@@ -32,7 +32,7 @@ export class ProductForm {
       <div class="card-form">
         <form id="form" action="/api/createProduct" enctype="multipart/form-data" method="POST" data-form>
           <div class="form-group">
-            <input class="form-control p-2" type="file" name="image" data-imageUrls multiple required autofocus>
+            <input class="form-control p-2" type="file" name="images" data-imageUrls multiple required autofocus>
           </div>
           <div class="form-group">
        <input class="form-control mt-3 p-2" type="text" placeholder="Nombre del producto" name="name" required data-name>
