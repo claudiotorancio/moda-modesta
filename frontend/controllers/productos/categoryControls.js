40139a9c9cdea42156fc302bdec6397ef758b4b4
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-09-03 22:46:41 -0300

    modificando visual modal 2

--- frontend/controllers/productos/categoryControls.js
+++ frontend/controllers/productos/categoryControls.js
@@ -4,6 +4,78 @@ export async function initializeCategoryControls() {
   volverBtn.textContent = "Volver";
   document.body.appendChild(volverBtn);
 
+  // Manejador para el evento 'popstate'
+  window.addEventListener("popstate", (event) => {
+    const state = event.state;
+    if (state && state.categoryId) {
+      // Manejar el caso en que se navega a una categoría específica
+      handleCategoryChange(state.categoryId);
+    } else {
+      // Manejar el caso en que se navega a la página principal
+      handleHomePage();
+    }
+  });
+
+  const handleCategoryChange = async (categoryId) => {
+    try {
+      const categoria = document.querySelector(`#${categoryId}`);
+      if (categoria) {
+        const categoriaBtn = categoria.querySelector("a");
+        const contenedorProductos = document.querySelector(
+          `[data-${categoryId}]`
+        );
+        const tarjetas = contenedorProductos.querySelectorAll(".card");
+
+        if (tarjetas.length === 0) {
+          contenedorProductos.innerHTML =
+            "<p>No hay productos para mostrar</p>";
+          return;
+        }
+
+        contenedorProductos.classList.add("allProducts");
+        tarjetas.forEach((tarjeta) => {
+          tarjeta.classList.add("allCard");
+        });
+
+        const imagen = contenedorProductos.querySelectorAll(".img-card");
+        imagen.forEach((tarjeta) => {
+          tarjeta.classList.add("img-allCard");
+        });
+
+        document.querySelectorAll(".productos").forEach((contenedor) => {
+          if (contenedor !== contenedorProductos) {
+            contenedor.innerHTML = "";
+          }
+        });
+
+        document.querySelectorAll(".categoria").forEach((categoria) => {
+          if (!categoria.querySelector(`[data-${categoryId}]`)) {
+            categoria.querySelector(".texto-categoria").style.display = "none";
+            categoria.querySelector(".productos").innerHTML = "";
+          }
+        });
+
+        categoriaBtn.textContent = "Volver";
+        volverBtn.classList.add("show");
+        window.scrollTo({ top: 330, behavior: "smooth" });
+      }
+    } catch (error) {
+      console.error("Error al obtener los productos:", error);
+    }
+  };
+
+  const handleHomePage = () => {
+    document.querySelectorAll(".productos").forEach((contenedor) => {
+      contenedor.innerHTML = "";
+    });
+
+    document.querySelectorAll(".categoria").forEach((categoria) => {
+      categoria.querySelector(".texto-categoria").style.display = "block";
+    });
+
+    volverBtn.classList.remove("show");
+  };
+
   document.querySelectorAll(".categoria").forEach((categoria) => {
     const categoriaBtn = categoria.querySelector("a");
     const opcion = categoriaBtn.getAttribute("id");
@@ -24,7 +96,6 @@ export async function initializeCategoryControls() {
 
         if (!enInicio) {
           contenedorProductos.classList.add("allProducts");
-
           tarjetas.forEach((tarjeta) => {
             tarjeta.classList.add("allCard");
           });
@@ -50,13 +121,13 @@ export async function initializeCategoryControls() {
 
           categoriaBtn.textContent = "Volver";
           enInicio = true;
-          volverBtn.classList.add("show"); // Mostrar el botón "Volver"
+          volverBtn.classList.add("show");
           window.scrollTo({ top: 330, behavior: "smooth" });
           history.pushState({ categoryId: opcion }, "", `#${opcion}`);
         } else {
-          volverBtn.classList.remove("show"); // Ocultar el botón "Volver"
-          history.pushState({}, "", "index.html"); // Usa pushState en lugar de replaceState
-          window.location.href = "index.html"; // Recargar la página opcionalmente
+          volverBtn.classList.remove("show");
+          history.pushState({}, "", "index.html");
+          window.location.href = "index.html";
         }
       } catch (error) {
         console.error("Error al obtener los productos:", error);
@@ -72,8 +143,7 @@ export async function initializeCategoryControls() {
     });
   });
 
-  // Agregar un evento de clic al botón "Volver"
-  volverBtn.addEventListener("click", async () => {
+  volverBtn.addEventListener("click", () => {
     history.pushState({}, "", "index.html");
     window.location.href = "index.html";
   });
