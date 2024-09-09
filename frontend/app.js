cb28f1d3f55bff8e29ec1ed54e36e6bd3513c063
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-09-09 01:09:49 -0300

    asociando carrito a la base de datos 1

--- frontend/app.js
+++ frontend/app.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   cargarReseñas();
 
   const listaServicesInstance = new ListaServices();
-  // const isAdmin = await listaServicesInstance.getAdmin();
+  const isAdmin = await listaServicesInstance.getAdmin();
   const user = JSON.parse(sessionStorage.getItem("user")) || null;
 
   const divUsuario = document.querySelector(".rounded-circle");
@@ -49,7 +49,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const titulo = document.querySelector("[data-titulo]");
 
   // Mostrar u ocultar elementos según si hay un usuario autenticado y es admin
-  if (user) {
+  if (user && isAdmin) {
     document.querySelectorAll(".admin-only").forEach((el) => {
       el.style.display = "block";
     });
