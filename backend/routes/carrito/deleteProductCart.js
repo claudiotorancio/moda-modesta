900eb4b39a096e92aedcf601267c9f4fc39e9a21
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-09-10 01:05:11 -0300

    corrigiendo muestra del modal carrito vacio cuando mustro un producto 15

--- backend/routes/carrito/deleteProductCart.js
+++ backend/routes/carrito/deleteProductCart.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 import MONGODB_URI from "../../config.js";
 import Cart from "../../models/Cart.js";
+import Vista from "../../models/Vista.js";
 
 const deleteProductCart = async (req, res) => {
   try {
-    // Obtener el ID del usuario a eliminar
     const _id = req.params.id;
 
     await mongoose.connect(MONGODB_URI, {
@@ -12,14 +12,34 @@ const deleteProductCart = async (req, res) => {
       useUnifiedTopology: true,
     });
 
-    // Buscar y eliminar el usuario por su ID
+    // Buscar y eliminar el producto del carrito por su ID
     const deletedProductCart = await Cart.findByIdAndDelete(_id);
 
-    // Usuario eliminado con éxito
-    return res.json({ message: "ProductCart deleted", deletedProductCart });
+    if (!deletedProductCart) {
+      return res
+        .status(404)
+        .json({ message: "Producto no encontrado en el carrito" });
+    }
+
+    // Actualizar el estado 'inCart' del producto en el modelo Vista
+    await Vista.updateOne(
+      { _id: deletedProductCart.productId },
+      { $set: { inCart: false } }
+    );
+
+    // Obtener información del producto actualizado
+    const updatedProduct = await Vista.findById(deletedProductCart.productId);
+
+    return res.json({
+      message: "ProductCart deleted and Vista updated",
+      deletedProductCart,
+      updatedProduct, // Devuelve información del producto actualizado
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Error en el servidor" });
+  } finally {
+    mongoose.connection.close();
   }
 };
 
