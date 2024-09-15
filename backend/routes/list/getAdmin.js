05208510379ac6eb6272485143efb12cb3ed529a
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-09-14 22:36:52 -0300

    dando a la fcion getAdmin tol de admin

--- backend/routes/list/getAdmin.js
+++ backend/routes/list/getAdmin.js
@@ -1,14 +1,14 @@
 const getAdmin = async (req, res) => {
   try {
     // Verificar si el usuario está autenticado
-    // if (!req.isAuthenticated()) {
-    //   return res.status(401).json({ ok: false });
-    // }
+    if (!req.isAuthenticated()) {
+      return res.status(401).json({ ok: false });
+    }
 
     // Verificar si el usuario tiene el rol de "admin"
-    // if (req.user.role !== "admin") {
-    //   return res.status(403).json({ ok: false });
-    // }
+    if (req.user.role !== "admin") {
+      return res.status(403).json({ ok: false });
+    }
 
     // Si el usuario está autenticado y tiene el rol de "admin", devolver true
     res.json({ ok: true });
