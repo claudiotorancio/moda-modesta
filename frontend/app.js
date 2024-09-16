b50af8de4e83231c33f7393b0ebaa266a2b1d556
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-09-15 22:00:12 -0300

    arreglando array en edicion 3

--- frontend/app.js
+++ frontend/app.js
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const titulo = document.querySelector("[data-titulo]");
 
   // Mostrar u ocultar elementos según si hay un usuario autenticado y es admin
-  if (user && isAdmin) {
+  if (user) {
     document.querySelectorAll(".admin-only").forEach((el) => {
       el.style.display = "block";
     });
