2256a12a6ecb910f93182fbb65cc22e964356843
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-09-16 16:37:31 -0300

    realizando prueba en productEditor con formData 13

--- frontend/controllers/productos/ProductEditor.js
+++ frontend/controllers/productos/ProductEditor.js
@@ -29,22 +29,23 @@ export class ProductEditor {
           ${imagePath
             .map(
               (path, index) => `
-            <div class="me-4 text-center" style="flex: 0 0 auto;">
-              <img class="img-fluid" style="max-width: 10rem;" src="${path}" alt="Imagen ${
+            <div class="me-3 text-center" style="flex: 0 0 auto;">
+              <img class="img-fluid" style="max-width: 8rem;" src="${path}" alt="Imagen ${
                 index + 1
               }">
               <p class="text-muted">Imagen ${index + 1}</p>
-            </div>`
+            </div>
+          `
             )
             .join("")}
         </div>
       </div>
-      <form id="form" enctype="multipart/form-data" method="POST" data-forma>
-        <p class="parrafo">Producto a editar</p>
+      <form action="/api/updateProduct/${id}" id="form" enctype="multipart/form-data" method="POST" data-forma>
+        <p class="parrafo">Editar Producto</p>
         ${imagePath
           .map(
-            (_, index) => `
-          <div class="form-group">
+            (path, index) => `
+          <div class="form-group mb-2">
             <input type="checkbox" id="image${
               index + 1
             }Check" class="form-check-input" name="image${
@@ -55,95 +56,118 @@ export class ProductEditor {
             }Check" class="form-check-label">Actualizar Imagen ${
               index + 1
             }</label>
-            <input class="form-control p-2 mt-2" placeholder="imageUrl" type="file" name="imagePath${
+            <input class="form-control p-1 mt-1" type="file" name="imagePath${
               index + 1
             }" data-image${index + 1} required disabled>
             <input type="hidden" class="oldImagePath" name="oldImagePath${
               index + 1
-            }" value="${imagePath[index]}" data-oldPath${index + 1}>
-          </div>`
+            }" value="${path}" data-oldPath${index + 1}>
+          </div>
+        `
           )
           .join("")}
-        <div class="form-group">
-          <input class="form-control mt-3 p-2" placeholder="nombre" type="text" value="${name}" required data-nombre>
+        <div class="form-group mb-2">
+          <input class="form-control p-1" placeholder="Nombre" type="text" value="${name}" required data-nombre>
         </div>
-        <div class="form-group">
-          <input class="form-control mt-3 mb-3 p-2" placeholder="precio" type="text" value="${price}" required data-precio>
+        <div class="form-group mb-2">
+          <input class="form-control p-1" placeholder="Precio" type="text" value="${price}" required data-precio>
         </div>
-        <div class="form-group">
-          <textarea class="form-control mt-3 mb-3 p-2" placeholder="Descripción" required data-description>${description}</textarea>
+        <div class="form-group mb-2">
+          <textarea class="form-control p-1" placeholder="Descripción" required data-description>${description}</textarea>
         </div>
-        <div class="form-group form-check mb-3">
+        <div class="form-group form-check mb-2">
           <input type="checkbox" class="form-check-input" id="isFeatured" name="isFeatured" ${
             isFeatured ? "checked" : ""
           }>
           <label class="form-check-label" for="isFeatured">Destacar producto</label>
         </div>
         <label for="sizes">Modificar talles y stocks</label>
-        <div class="form-group mb-4">
+        <div class="form-group mb-3">
           ${this.renderSizeOptions(sizes)}
         </div>
-        <button type="submit" class="btn btn-primary btn-lg">Editar producto</button>
+        <button type="submit" class="btn btn-primary btn-sm">Editar producto</button>
       </form>
     `;
 
     this.productoEdicion.classList.add("modalVisor");
+
     this.setupImageCheckListeners();
   }
 
   renderSizeOptions(selectedSizes = []) {
-    const sizes = ["Talle 1", "Talle 2", "Talle 3", "Talle 4", "Talle 5"];
+    const sizes = [
+      { size: "Talle 1" },
+      { size: "Talle 2" },
+      { size: "Talle 3" },
+      { size: "Talle 4" },
+      { size: "Talle 5" },
+    ];
 
     return sizes
       .map(
-        (size) => `
-      <div class="form-check-inline me-3">
-        <input class="form-check-input" type="checkbox" value="${size}" name="sizes" id="${size
-          .replace(" ", "")
-          .toLowerCase()}" ${
-          selectedSizes.some((s) => s.size === size) ? "checked" : ""
-        }>
-        <label class="form-check-label" for="${size
-          .replace(" ", "")
-          .toLowerCase()}">${size}</label>
-        <input class="form-control mt-2" type="number" placeholder="Sin stock" name="stock_${size
-          .replace(" ", "")
-          .toLowerCase()}" value="${
-          selectedSizes.find((s) => s.size === size)?.stock || 0
-        }" disabled>
-      </div>`
+        ({ size }) => `
+      <div class="form-check form-check-inline me-2">
+        <input 
+          class="form-check-input" 
+          type="checkbox" 
+          value="${size}" 
+          name="sizes" 
+          id="${size.replace(" ", "").toLowerCase()}" 
+          ${selectedSizes.some((s) => s.size === size) ? "checked" : ""}
+        >
+        <label 
+          class="form-check-label" 
+          for="${size.replace(" ", "").toLowerCase()}">
+          ${size}
+        </label>
+        <input 
+          class="form-control form-control-sm mt-1" 
+          type="number" 
+          placeholder="Sin stock" 
+          name="stock_${size.replace(" ", "").toLowerCase()}" 
+          value="${selectedSizes.find((s) => s.size === size)?.stock || 0}" 
+          disabled
+        >
+      </div>
+    `
       )
       .join("");
   }
 
   setupImageCheckListeners() {
-    const maxSelectable = 2;
     const imageChecks = document.querySelectorAll(
       "[data-check-image1], [data-check-image2], [data-check-image3]"
     );
     const imageInputs = document.querySelectorAll(
       "[data-image1], [data-image2], [data-image3]"
     );
+    const maxSelectable = 2;
 
-    imageInputs.forEach(
-      (input, index) => (input.disabled = !imageChecks[index].checked)
-    );
+    // Deshabilitar los inputs de imagen inicialmente
+    imageInputs.forEach((input, index) => {
+      input.disabled = !imageChecks[index].checked;
+    });
 
     imageChecks.forEach((check, index) => {
       check.addEventListener("change", () => {
+        // Contar cuántas imágenes han sido seleccionadas
         const selectedImages = Array.from(imageChecks).filter(
           (c) => c.checked
         ).length;
+
         if (selectedImages >= maxSelectable) {
           alert(
             "Solo puedes reemplazar una imagen. Para reemplazar ambas, crea un nuevo producto."
           );
           check.checked = false;
         }
+
+        // Habilitar/deshabilitar el input de imagen correspondiente
         imageInputs[index].disabled = !check.checked;
       });
     });
 
+    // Habilitar/Deshabilitar inputs de stock basados en el estado del checkbox
     const sizeChecks = document.querySelectorAll('input[name="sizes"]');
     const stockInputs = document.querySelectorAll('input[name^="stock_"]');
 
@@ -152,10 +176,9 @@ export class ProductEditor {
       const stockInput = document.querySelector(
         `input[name="stock_${size.replace(" ", "").toLowerCase()}"]`
       );
-      check.addEventListener(
-        "change",
-        () => (stockInput.disabled = !check.checked)
-      );
+      check.addEventListener("change", () => {
+        stockInput.disabled = !check.checked;
+      });
     });
   }
 
@@ -167,6 +190,12 @@ export class ProductEditor {
 
       const imageInput1 = document.querySelector("[data-image1]");
       const imageInput2 = document.querySelector("[data-image2]");
+
+      if (!imageInput1 && !imageInput2) {
+        console.error("No se encontraron los elementos de entrada de imagen.");
+        return;
+      }
+
       const imagePath1 = imageInput1 ? imageInput1.files[0] : null;
       const imagePath2 = imageInput2 ? imageInput2.files[0] : null;
 
