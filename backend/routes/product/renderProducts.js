6a4c68b82c59d8c72c5e15481e46955c3b95c7fb
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-09-17 21:24:24 -0300

    agregando logica para el manejo del stock 1

--- backend/routes/product/renderProducts.js
+++ backend/routes/product/renderProducts.js
@@ -7,7 +7,7 @@ const renderProducts = async (req, res) => {
   console.log(req.user);
   try {
     //Relacionar id de usuario con producto para visualizar solo sus productos
-    // const user_id = req.user._id;
+    const user_id = req.user._id;
 
     // Conectar a la base de datos mediante serverless function
     await mongoose.connect(MONGODB_URI, {
@@ -17,18 +17,18 @@ const renderProducts = async (req, res) => {
 
     //Verificar si el usuario esta autenticado
 
-    // const usuarioAdmin = req.user.role === "admin";
-    // console.log(usuarioAdmin);
+    const usuarioAdmin = req.user.role === "admin";
+    console.log(usuarioAdmin);
 
     let products;
     //Verificar si el usuario es administrador
-    // if (req.user.role === "admin") {
-    // Si es administrador, buscar productos utilizando el modelo Vista
-    products = await Vista.find();
-    // } else {
-    //   // Si no es administrador, buscar productos utilizando el modelo Product
-    //   products = await Product.find({ user_id: user_id });
-    // }
+    if (req.user.role === "admin") {
+      // Si es administrador, buscar productos utilizando el modelo Vista
+      products = await Vista.find({ user_id: user_id });
+    } else {
+      // Si no es administrador, buscar productos utilizando el modelo Product
+      products = await Product.find({ user_id: user_id });
+    }
 
     res.json({
       products,
