df26183721052fa8c9a116453ecdbd278a67c916
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-09-27 11:38:55 -0300

    cookiew-session 12

--- backend/routes/login/signin.js
+++ backend/routes/login/signin.js
@@ -2,11 +2,11 @@ import passport from "../../lib/passport.js";
 
 const signin = async (req, res) => {
   try {
-    // Usar passport.authenticate() para manejar la autenticación del usuario
+    // Utilizar passport.authenticate() para manejar la autenticación del usuario
     passport.authenticate("local.signin", { session: false })(
       req,
       res,
-      async (error, user) => {
+      async (error) => {
         if (error) {
           console.error("Error al autenticar la sesión del usuario:", error);
           return res
@@ -14,26 +14,16 @@ const signin = async (req, res) => {
             .json({ error: "Error al autenticar la sesión del usuario" });
         }
 
-        // Si la autenticación falla
-        if (!user) {
-          return res
-            .status(401)
-            .json({ message: "Usuario o contraseña incorrectos" });
+        // Verificar si el usuario está autenticado
+        if (!req.isAuthenticated()) {
+          return res.status(401).json({ message: "Usuario no autenticado" });
         }
 
-        // Si la autenticación es exitosa, ahora llamamos a req.login para iniciar la sesión
-        req.login(user, (err) => {
-          if (err) {
-            console.error("Error al iniciar sesión:", err);
-            return res.status(500).json({ error: "Error al iniciar sesión" });
-          }
+        console.log("Usuario autenticado:", req.user);
+        console.log("Sesión:", req.session);
 
-          console.log("Usuario autenticado:", req.user);
-          console.log("Sesión:", req.session);
-
-          // Manejo de la respuesta para mostrar en pantalla los valores
-          return res.json({ user: req.user });
-        });
+        // Manejo de la respuesta para mostrar en pantalla los valores
+        return res.json({ user: req.user });
       }
     );
   } catch (error) {
