fc2aa76498deae599cbb65f0ba155cba14304a0f
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-09-27 03:14:09 -0300

    reparo problema con cookie secure 14

--- backend/lib/auth.js
+++ backend/lib/auth.js
@@ -1,18 +1,17 @@
 import jwt from "jsonwebtoken";
 import Users from "../models/User.js"; // Importa el modelo de usuario
+import { connectToDatabase } from "../config.js"; // Asegúrate de importar tu función para conectar a la base de datos
 
-export const authenticateJWT = async (req, res, user, next) => {
+export const authenticateJWT = async (req, res, next) => {
   try {
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    res.cookie("user_sid", token, { httpOnly: true });
+    // Obtén el token de las cookies
+    const token = req.cookies.user_sid; // Cambia esto si usas otro nombre para la cookie
 
     if (token) {
       jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
         if (err) {
           console.error("Error en la verificación del token:", err);
-          return res.status(403).send("Forbidden"); // Si el token es inválido
+          return res.status(403).json({ ok: false, message: "Forbidden" }); // Si el token es inválido
         }
 
         console.log("Token decodificado:", decoded);
@@ -26,21 +25,25 @@ export const authenticateJWT = async (req, res, user, next) => {
 
           if (!user || !user.active) {
             // Si el usuario no existe o no está activo
-            return res.status(403).send("Forbidden");
+            return res.status(403).json({ ok: false, message: "Forbidden" });
           }
 
           req.user = user; // Adjunta el usuario a la solicitud (request)
-          return res.json({ ok: true });
+          return res.json({ ok: true }); // Devuelve ok: true
         } catch (dbError) {
           console.error("Error en la base de datos:", dbError);
-          return res.status(500).send("Internal Server Error"); // Si hay un error con la base de datos
+          return res
+            .status(500)
+            .json({ ok: false, message: "Internal Server Error" }); // Si hay un error con la base de datos
         }
       });
     } else {
-      return res.status(401).send("Unauthorized"); // Si no hay token
+      return res.status(401).json({ ok: false, message: "Unauthorized" }); // Si no hay token
     }
   } catch (error) {
     console.error("Error en la autenticación:", error);
-    return res.status(500).send("Internal Server Error"); // Error general
+    return res
+      .status(500)
+      .json({ ok: false, message: "Internal Server Error" }); // Error general
   }
 };
