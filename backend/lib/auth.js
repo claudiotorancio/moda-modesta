15e2c5819b9feb622599d54f409c943e734a3bb4
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-09-27 00:58:04 -0300

    agrego logica de jwt

--- backend/lib/auth.js
+++ backend/lib/auth.js
@@ -1,15 +1,29 @@
-// backend/middleware/auth.js
 import jwt from "jsonwebtoken";
+import User from "../models/User.js"; // Asegúrate de importar tu modelo de usuario
 
-export const authenticateJWT = (req, res, next) => {
+export const authenticateJWT = async (req, res, next) => {
   const token = req.cookies.user_sid; // Ajusta esto si guardas el token en otro lugar
   if (token) {
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
       if (err) {
         return res.sendStatus(403); // Forbidden
       }
-      req.user = user; // Agregar el usuario al request
-      next();
+
+      // Verifica si el usuario existe y está activo en la base de datos
+      try {
+        const user = await User.findById(decoded.id); // Aquí asumimos que el token contiene el ID del usuario
+
+        if (!user || !user.active) {
+          // Revisa si el usuario existe y está activo
+          return res.sendStatus(403); // Forbidden
+        }
+
+        req.user = user; // Agregar el usuario al request
+        next();
+      } catch (dbError) {
+        console.error(dbError);
+        return res.sendStatus(500); // Internal Server Error
+      }
     });
   } else {
     res.sendStatus(401); // Unauthorized
