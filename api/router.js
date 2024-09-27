d384c3f16ba66917df0b473e37e7e7d58013caca
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-09-27 00:28:28 -0300

    agrego logica rateLimiter 4

--- api/router.js
+++ api/router.js
@@ -77,9 +77,9 @@ router.use(
     }),
     cookie: {
       expires: 600000,
-      secure: true, // Asegúrate de que esto esté habilitado
-      // httpOnly: true, // Previene acceso JavaScript a la cookie
-      // sameSite: "lax", // Proteger contra CSRF
+      // secure: true, // Asegúrate de que esto esté habilitado
+      httpOnly: true, // Previene acceso JavaScript a la cookie
+      sameSite: "lax", // Proteger contra CSRF
     },
   })
 );
