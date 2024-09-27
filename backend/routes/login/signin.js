31c879ea90179e8dbd45caaa9280fabe62d4e258
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-09-27 11:44:07 -0300

    cookiew-session 14

--- backend/routes/login/signin.js
+++ backend/routes/login/signin.js
@@ -3,29 +3,25 @@ import passport from "../../lib/passport.js";
 const signin = async (req, res) => {
   try {
     // Utilizar passport.authenticate() para manejar la autenticación del usuario
-    passport.authenticate("local.signin", { session: true })(
-      req,
-      res,
-      async (error) => {
-        if (error) {
-          console.error("Error al autenticar la sesión del usuario:", error);
-          return res
-            .status(500)
-            .json({ error: "Error al autenticar la sesión del usuario" });
-        }
-
-        // Verificar si el usuario está autenticado
-        if (!req.isAuthenticated()) {
-          return res.status(401).json({ message: "Usuario no autenticado" });
-        }
-
-        console.log("Usuario autenticado:", req.user);
-        console.log("Sesión:", req.session);
-
-        // Manejo de la respuesta para mostrar en pantalla los valores
-        return res.json({ user: req.user });
+    passport.authenticate("local.signin")(req, res, async (error) => {
+      if (error) {
+        console.error("Error al autenticar la sesión del usuario:", error);
+        return res
+          .status(500)
+          .json({ error: "Error al autenticar la sesión del usuario" });
       }
-    );
+
+      // Verificar si el usuario está autenticado
+      if (!req.isAuthenticated()) {
+        return res.status(401).json({ message: "Usuario no autenticado" });
+      }
+
+      console.log("Usuario autenticado:", req.user);
+      console.log("Sesión:", req.session);
+
+      // Manejo de la respuesta para mostrar en pantalla los valores
+      return res.json({ user: req.user });
+    });
   } catch (error) {
     console.error("Error en la función signin:", error);
     res.status(500).json({ message: "Error interno del servidor" });
