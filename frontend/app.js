f246e00a83afc4deb8c43b81f70199a864607d5a
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-10-01 12:35:06 -0300

    agrego logica signup 10

--- frontend/app.js
+++ frontend/app.js
@@ -68,7 +68,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   // Mostrar u ocultar elementos según si hay un usuario autenticado y es admin
 
-  if (isAdmin) {
+  if (isAdmin.role === "admin") {
     document.querySelectorAll(".admin-only").forEach((el) => {
       el.style.display = "block";
     });
