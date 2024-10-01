c2cd28c9e19420dcce4f9e6b802e0f98c91797c6
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-10-01 16:29:40 -0300

    agregando botones a verProductos en admin 1

--- frontend/app-ver-productos.js
+++ frontend/app-ver-productos.js
@@ -16,12 +16,39 @@ import { cargarReseñas } from "./controllers/reseña/reseñas.js";
 import { initializeCategoryControls } from "./controllers/productos/categoryControls.js";
 import { ListaServices } from "./services/lista_services.js";
 import { hashControllers } from "./controllers/hashControllers.js";
+import { FormResena } from "./controllers/reseña/formResena.js";
+import { Estilos } from "./controllers/admin/Estilos.js";
+import {
+  loadBannerImage,
+  loadBannerText,
+  loadColorSettings,
+  loadColorSettingsCard,
+  loadLogoImage,
+} from "./controllers/admin/eventBanner.js";
+import { buscar } from "./controllers/buscador/buscador.js";
+import { RenderStock } from "./controllers/stock/RenderStock.js";
+import { ocultarProductos } from "./controllers/ocultarProductos/ocultarProductos.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   const hash = window.location.hash;
   if (hash.startsWith("#product-")) {
     await hashControllers();
   }
+
+  loadBannerText();
+  loadLogoImage();
+  loadBannerImage();
+  loadColorSettings();
+  loadColorSettingsCard();
+
+  // const contactUser = document.querySelector("[data-contact]");
+  const resenas = document.querySelector("[data-resenas]");
+  const ventas = document.querySelector("[data-ventas]");
+  const estilosDiseno = document.querySelector("[data-estilosDiseno]");
+  const susxriptores = document.querySelector("[data-suscriptores]");
+  const listaStock = document.querySelector("[data-stock]");
+
+  const titulo = document.querySelector("[data-titulo]");
 
   initializeCategoryControls();
   const listaServicesInstance = new ListaServices();
@@ -41,6 +68,53 @@ document.addEventListener("DOMContentLoaded", async () => {
     actualizarUsuario.textContent = `${user}`;
     logoutUsuario.innerHTML = '<i class="fa-solid fa-right-from-bracket"></i>';
     userActive.style.display = "none";
+    const envio = document.querySelector("[data-pedidos]");
+    envio.addEventListener("click", (e) => {
+      e.preventDefault();
+      ocultarProductos();
+      const comprasInstance = new Compras(titulo);
+      comprasInstance.renderLista();
+    });
+
+    const crearProducto = document.querySelector("[data-crearProductos]");
+    crearProducto.addEventListener("click", async (e) => {
+      e.preventDefault();
+      const productForm = new ProductForm(titulo);
+      await productForm.render();
+    });
+
+    resenas.addEventListener("click", (e) => {
+      e.preventDefault();
+      ocultarProductos();
+      const formResena = new FormResena(titulo);
+      formResena.render();
+    });
+
+    estilosDiseno.addEventListener("click", (e) => {
+      e.preventDefault();
+      const estilosInstance = new Estilos(titulo);
+      estilosInstance.render();
+    });
+
+    listaStock.addEventListener("click", async (e) => {
+      e.preventDefault();
+      ocultarProductos();
+      const stockInstance = new RenderStock(titulo);
+      await stockInstance.render();
+    });
+
+    susxriptores.addEventListener("click", async (e) => {
+      e.preventDefault();
+      ocultarProductos();
+      const ListaControllersInstamce = new ListaControllers(titulo);
+      await ListaControllersInstamce.renderLista();
+    });
+
+    ventas.addEventListener("click", (e) => {
+      e.preventDefault();
+      alert("Seccion en Construccion");
+    });
+    buscar();
   }
 
   cargarReseñas();
