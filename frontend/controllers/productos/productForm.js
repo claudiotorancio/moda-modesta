7906f6172e95bc82688eead265cb364707150431
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-10-09 21:00:18 -0300

    comienzo a dar forma a los productos diversos 1

--- frontend/controllers/productos/productForm.js
+++ frontend/controllers/productos/productForm.js
@@ -33,113 +33,89 @@ export class ProductForm {
     card.className = "d-flex justify-content-center align-items-center"; // Centrar el contenedor del formulario
 
     card.innerHTML = `
-      <div>
-        <div class="text-center">
-          <div class="card-header mb-2">
-            <h3 style="font-weight: bold;">Agregar producto</h3>
-          </div>
-          <p>Modo seleccion de imagenes:</p>
-          <p>a- Seleccionar una sola imagen</p>
-          <p>b- Seleccionar varias imagenes de una vez para efecto carrousel.</p>
-          <br>
-          <div class="card-body w-100">
-            <form id="form" action="/api/createProduct" enctype="multipart/form-data" method="POST" data-form>
-              <!-- Campos del formulario -->
-              <div class="form-group">
-                <input class="form-control p-2" type="file" name="images" data-imageUrls multiple required autofocus>
-              </div>
-              <div class="form-group">
-                <input class="form-control mt-3 p-2" type="text" placeholder="Nombre del producto" name="name" required data-name>
-              </div>
-              <div class="form-group">
-                <input class="form-control mt-3 mb-3 p-2" type="text" placeholder="Precio del producto" name="price" required data-price>
-              </div>
-              <div class="form-group">
-                <textarea class="form-control mt-3 mb-3 p-2" placeholder="Descripción" name="description" required data-description></textarea>
-              </div>
-              <p for="miMenuDesplegable">Sección</p>
-              <div class="form-group">
-                <select class="form-control mb-3 p-2" id="miMenuDesplegable" name="section">
-                  <option value="opcion1">Vestidos</option>
-                  <option value="opcion2">Polleras</option>
-                  <option value="opcion3">Diversos</option>
-                </select>
-              </div >
-              
-              <!-- Talles disponibles y cantidad de stock -->
-              <label for="variation_1">Talles y stock disponibles</label>
-              <div class="form-group mb-4" id="sizesContainer">
-                <div class="form-row">
-                  <div class="col-center flex">
-                    <div class="form-check-inline nt-2 me-3">
-                      <input class="form-check-input" type="checkbox" value="Talle 1" name="sizes" id="talle1">
-                      <label class="form-check-label" for="talle1">Talle 1</label>
-                      <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle1>
-                    </div>
-                     <div class="form-check-inline me-3">
-                      <input class="form-check-input" type="checkbox" value="Talle 2" name="sizes" id="talle2">
-                      <label class="form-check-label" for="talle2">Talle 2</label>
-                      <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle2>
-                    </div>
-                     <div class="form-check-inline me-3">
-                      <input class="form-check-input" type="checkbox" value="Talle 3" name="sizes" id="talle3">
-                      <label class="form-check-label" for="talle3">Talle 3</label>
-                      <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle3>
-                    </div>
-                     <div class="form-check-inline me-3">
-                      <input class="form-check-input" type="checkbox" value="Talle 4" name="sizes" id="talle4">
-                      <label class="form-check-label" for="talle4">Talle 4</label>
-                      <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle4>
-                    </div>
-                     <div class="form-check-inline me-3">
-                      <input class="form-check-input" type="checkbox" value="Talle 5" name="sizes" id="talle5">
-                      <label class="form-check-label" for="talle5">Talle 5</label>
-                      <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle5>
-                    </div>
+    <div>
+      <div class="text-center">
+        <div class="card-header mb-2">
+          <h3 style="font-weight: bold;">Agregar producto</h3>
+        </div>
+        <p>Modo seleccion de imagenes:</p>
+        <p>a- Seleccionar una sola imagen</p>
+        <p>b- Seleccionar varias imagenes de una vez para efecto carrousel.</p>
+        <br>
+        <div class="card-body w-100">
+          <form id="form" action="/api/createProduct" enctype="multipart/form-data" method="POST" data-form>
+            <!-- Campos del formulario -->
+            <!-- Imágenes, nombre, precio, descripción, sección -->
+            <div class="form-group">
+              <input class="form-control p-2" type="file" name="images" data-imageUrls multiple required autofocus>
+            </div>
+            <div class="form-group">
+              <input class="form-control mt-3 p-2" type="text" placeholder="Nombre del producto" name="name" required data-name>
+            </div>
+            <div class="form-group">
+              <input class="form-control mt-3 mb-3 p-2" type="text" placeholder="Precio del producto" name="price" required data-price>
+            </div>
+            <div class="form-group">
+              <textarea class="form-control mt-3 mb-3 p-2" placeholder="Descripción" name="description" required data-description></textarea>
+            </div>
+            <p for="miMenuDesplegable">Sección</p>
+            <div class="form-group">
+              <select class="form-control mb-3 p-2" id="miMenuDesplegable" name="section">
+                <option value="opcion1">Vestidos</option>
+                <option value="opcion2">Polleras</option>
+                <option value="opcion3">Diversos</option>
+              </select>
+            </div >
+            
+            <!-- Talles disponibles y cantidad de stock -->
+            <label for="variation_1">Talles y stock disponibles</label>
+            <div class="form-group mb-4">
+              <div class="form-row">
+                <div class="col-center flex">
+                  <div class="form-check-inline nt-2 me-3">
+                    <input class="form-check-input" type="checkbox" value="Talle 1" name="sizes" id="talle1">
+                    <label class="form-check-label" for="talle1">Talle 1</label>
+                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle1>
+                  </div>
+                   <div class="form-check-inline me-3">
+                    <input class="form-check-input" type="checkbox" value="Talle 2" name="sizes" id="talle2">
+                    <label class="form-check-label" for="talle2">Talle 2</label>
+                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle2>
+                  </div>
+                   <div class="form-check-inline me-3">
+                    <input class="form-check-input" type="checkbox" value="Talle 3" name="sizes" id="talle3">
+                    <label class="form-check-label" for="talle3">Talle 3</label>
+                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle3>
+                  </div>
+                   <div class="form-check-inline me-3">
+                    <input class="form-check-input" type="checkbox" value="Talle 4" name="sizes" id="talle4">
+                    <label class="form-check-label" for="talle4">Talle 4</label>
+                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle4>
+                  </div>
+                   <div class="form-check-inline me-3">
+                    <input class="form-check-input" type="checkbox" value="Talle 5" name="sizes" id="talle5">
+                    <label class="form-check-label" for="talle5">Talle 5</label>
+                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle5>
                   </div>
                 </div>
               </div>
+            </div>
 
-              <!-- Checkbox para destacar producto -->
-              <div class="form-group">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" id="isFeatured" name="isFeatured">
-                  <label class="form-check-label" for="isFeatured">Destacar producto</label>
-                </div>
+            <!-- Checkbox para destacar producto -->
+            <div class="form-group">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="isFeatured" name="isFeatured">
+                <label class="form-check-label" for="isFeatured">Destacar producto</label>
               </div>
+            </div>
 
-              <button type="submit" class="btn btn-primary btn-lg">Agregar</button>
-            </form>
-          </div>
+            <button type="submit" class="btn btn-primary btn-lg">Agregar</button>
+          </form>
         </div>
       </div>
-    `;
-
-    // Agregar evento para mostrar/ocultar talles
-    this.toggleSizesVisibility();
-
+    </div>
+  `;
     return card;
-  }
-
-  toggleSizesVisibility() {
-    const sectionSelect = document.getElementById("miMenuDesplegable");
-    const sizesContainer = document.getElementById("sizesContainer");
-
-    // Función para mostrar/ocultar talles
-    const updateSizesVisibility = () => {
-      if (sectionSelect.value === "opcion3") {
-        // Diversos
-        sizesContainer.style.display = "none";
-      } else {
-        sizesContainer.style.display = "block";
-      }
-    };
-
-    // Actualizar visibilidad al cargar
-    updateSizesVisibility();
-
-    // Agregar evento para el cambio en el select
-    sectionSelect.addEventListener("change", updateSizesVisibility);
   }
 
   // Capturar el evento submit
