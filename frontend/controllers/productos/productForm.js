56f21cccd6084afeb670b1f998cf916532b42e97
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-10-18 15:26:25 -0300

    arreglo froductForm

--- frontend/controllers/productos/productForm.js
+++ frontend/controllers/productos/productForm.js
@@ -26,59 +26,97 @@ export class ProductForm {
   // Crear formulario dinámico
   createForm() {
     const card = document.createElement("div");
-    card.classList.add("card", "card-body", "p-3");
+    card.className = "d-flex justify-content-center align-items-center"; // Centrar el contenedor del formulario
 
     card.innerHTML = `
-      <form class="formulario" method="POST" enctype="multipart/form-data">
-        <div class="mb-3">
-          <label>Nombre del Producto</label>
-          <input class="form-control p-2" type="text" name="name" data-name required autofocus />
+    <div>
+      <div class="text-center">
+        <div class="card-header mb-2">
+          <h3 style="font-weight: bold;">Agregar producto</h3>
         </div>
-        <div class="mb-3">
-          <label>Precio</label>
-          <input class="form-control p-2" type="number" step="0.01" name="price" data-price required />
+        <p>Modo seleccion de imagenes:</p>
+        <p>a- Seleccionar una sola imagen</p>
+        <p>b- Seleccionar dos (2) imagenes de una vez para efecto carrousel.</p>
+        <br>
+        <div class="card-body w-100">
+          <form id="form" action="/api/createProduct" enctype="multipart/form-data" method="POST" data-form>
+
+            <p for="miMenuDesplegable">Sección</p>
+            <div class="form-group">
+              <select class="form-control mb-3 p-2" id="miMenuDesplegable" name="section">
+                <option value="opcion1">Vestidos</option>
+                <option value="opcion2">Polleras</option>
+                <option value="opcion3">Diversos</option>
+              </select>
+            </div>
+
+            <div class="form-group">
+              <input class="form-control p-2" type="file" name="images" data-imageUrls multiple required autofocus accept="image/*" max="2">
+            </div>
+            <div class="form-group">
+              <input class="form-control mt-3 p-2" type="text" placeholder="Nombre del producto" name="name" required data-name>
+            </div>
+            <div class="form-group">
+              <input class="form-control mt-3 mb-3 p-2" type="text" placeholder="Precio del producto" name="price" required data-price>
+            </div>
+            <div class="form-group">
+              <textarea class="form-control mt-3 mb-3 p-2" placeholder="Descripción" name="description" required data-description></textarea>
+            </div>
+            
+            <!-- Talles disponibles y cantidad de stock -->
+         
+            <div class="form-group mb-4 sizes-container"> <!-- Añade una clase aquí -->
+               <label for="variation_1">Talles y stock disponibles</label>
+              <div class="form-row">
+                <div class="col-center flex">
+                  <div class="form-check-inline nt-2 me-3">
+                    <input class="form-check-input" type="checkbox" value="Talle 1" name="sizes" id="talle1">
+                    <label class="form-check-label" for="talle1">Talle 1</label>
+                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle1>
+                  </div>
+                  <div class="form-check-inline me-3">
+                    <input class="form-check-input" type="checkbox" value="Talle 2" name="sizes" id="talle2">
+                    <label class="form-check-label" for="talle2">Talle 2</label>
+                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle2>
+                  </div>
+                  <div class="form-check-inline me-3">
+                    <input class="form-check-input" type="checkbox" value="Talle 3" name="sizes" id="talle3">
+                    <label class="form-check-label" for="talle3">Talle 3</label>
+                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle3>
+                  </div>
+                  <div class="form-check-inline me-3">
+                    <input class="form-check-input" type="checkbox" value="Talle 4" name="sizes" id="talle4">
+                    <label class="form-check-label" for="talle4">Talle 4</label>
+                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle4>
+                  </div>
+                  <div class="form-check-inline me-3">
+                    <input class="form-check-input" type="checkbox" value="Talle 5" name="sizes" id="talle5">
+                    <label class="form-check-label" for="talle5">Talle 5</label>
+                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle5>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group general-stock-container d-none"> <!-- Oculto por defecto -->
+  <label for="generalStock">Stock general</label>
+  <input type="number" min="0" class="form-control mt-2" placeholder="Stock" id="generalStock" name="generalStock">
+</div>
+
+
+            <div class="form-group">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="isFeatured" name="isFeatured">
+                <label class="form-check-label" for="isFeatured">Destacar producto</label>
+              </div>
+            </div>
+
+            <button type="submit" class="btn btn-primary btn-lg">Agregar</button>
+          </form>
         </div>
-        <div class="mb-3">
-          <label>Descripción</label>
-          <textarea class="form-control p-2" name="description" data-description required></textarea>
-        </div>
-        <div class="mb-3">
-          <label>Imágenes</label>
-          <input class="form-control p-2" type="file" name="images" data-imageUrls multiple required autofocus accept="image/*" max="2" />
-        </div>
-        <div class="mb-3">
-          <label>Sección</label>
-          <select id="miMenuDesplegable" class="form-control p-2">
-            <option value="opcion1">Opción 1</option>
-            <option value="opcion2">Opción 2</option>
-            <option value="opcion3">Opción 3</option>
-          </select>
-        </div>
-        <div id="sizesSection" class="mb-3">
-          <label>Selecciona los talles disponibles:</label>
-          <div>
-            <label><input type="checkbox" name="sizes" value="XS"> XS</label>
-            <input type="number" placeholder="Stock XS" data-stock-xs style="display: none;" />
-            <label><input type="checkbox" name="sizes" value="S"> S</label>
-            <input type="number" placeholder="Stock S" data-stock-s style="display: none;" />
-            <label><input type="checkbox" name="sizes" value="M"> M</label>
-            <input type="number" placeholder="Stock M" data-stock-m style="display: none;" />
-            <label><input type="checkbox" name="sizes" value="L"> L</label>
-            <input type="number" placeholder="Stock L" data-stock-l style="display: none;" />
-            <label><input type="checkbox" name="sizes" value="XL"> XL</label>
-            <input type="number" placeholder="Stock XL" data-stock-xl style="display: none;" />
-          </div>
-        </div>
-        <div id="generalStockSection" class="mb-3" style="display: none;">
-          <label>Stock General</label>
-          <input class="form-control p-2" type="number" id="generalStock" name="generalStock" />
-        </div>
-        <div class="mb-3">
-          <label><input type="checkbox" id="isFeatured" /> Producto destacado</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Crear Producto</button>
-      </form>
-    `;
+      </div>
+    </div>
+  `;
     return card;
   }
 
