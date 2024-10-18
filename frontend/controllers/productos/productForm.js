27f884ee6d83ed25aef99b605526cdfcb3ebb598
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-10-18 14:48:07 -0300

    arreglo froductForm

--- frontend/controllers/productos/productForm.js
+++ frontend/controllers/productos/productForm.js
@@ -1,220 +1,161 @@
 import productoServices from "../../services/product_services.js";
-import { controllers } from "./productos_controllers.js";
 
 export class ProductForm {
   constructor(titulo) {
     this.titulo = titulo;
+    this.sectionSelect = null; // Referencia al select de sección
+    this.sectionSelectHandler = null; // Almacena el manejador del evento select
     this.render();
-    this.setupSelectChangeHandler();
-    this.updateSizesVisibility(); // Llama a la función al iniciar para ocultar talles si es necesario
   }
 
-  // Mostrar formulario
   render() {
     this.clearForm();
     const card = this.createForm();
     this.titulo.appendChild(card);
 
+    this.sectionSelect = this.titulo.querySelector("#miMenuDesplegable");
     this.setupSelectChangeHandler();
-    this.updateSizesVisibility(); // Llama a la función al iniciar para ocultar talles si es necesario
+    this.updateSizesVisibility();
     this.setupFormSubmitHandler();
   }
 
-  mostrarProducts() {
-    document.querySelectorAll(".categoria").forEach((categoria) => {
-      categoria.querySelector(".texto-categoria").style.display = "flex";
-      controllers.renderProducts();
-    });
-  }
-
-  // Vaciar contenido
   clearForm() {
     this.titulo.innerHTML = "";
   }
 
-  // Crear formulario dinámico
   createForm() {
     const card = document.createElement("div");
-    card.className = "d-flex justify-content-center align-items-center"; // Centrar el contenedor del formulario
+    card.classList.add("card", "card-body", "p-3");
 
     card.innerHTML = `
-    <div>
-      <div class="text-center">
-        <div class="card-header mb-2">
-          <h3 style="font-weight: bold;">Agregar producto</h3>
+      <form class="formulario" method="POST" enctype="multipart/form-data">
+        <div class="mb-3">
+          <label>Nombre del Producto</label>
+          <input class="form-control p-2" type="text" name="name" data-name required autofocus />
         </div>
-        <p>Modo seleccion de imagenes:</p>
-        <p>a- Seleccionar una sola imagen</p>
-        <p>b- Seleccionar dos (2) imagenes de una vez para efecto carrousel.</p>
-        <br>
-        <div class="card-body w-100">
-          <form id="form" action="/api/createProduct" enctype="multipart/form-data" method="POST" data-form>
-
-            <p for="miMenuDesplegable">Sección</p>
-            <div class="form-group">
-              <select class="form-control mb-3 p-2" id="miMenuDesplegable" name="section">
-                <option value="opcion1">Vestidos</option>
-                <option value="opcion2">Polleras</option>
-                <option value="opcion3">Diversos</option>
-              </select>
-            </div>
-
-            <div class="form-group">
-              <input class="form-control p-2" type="file" name="images" data-imageUrls multiple required autofocus>
-            </div>
-            <div class="form-group">
-              <input class="form-control mt-3 p-2" type="text" placeholder="Nombre del producto" name="name" required data-name>
-            </div>
-            <div class="form-group">
-              <input class="form-control mt-3 mb-3 p-2" type="text" placeholder="Precio del producto" name="price" required data-price>
-            </div>
-            <div class="form-group">
-              <textarea class="form-control mt-3 mb-3 p-2" placeholder="Descripción" name="description" required data-description></textarea>
-            </div>
-            
-            <!-- Talles disponibles y cantidad de stock -->
-         
-            <div class="form-group mb-4 sizes-container"> <!-- Añade una clase aquí -->
-               <label for="variation_1">Talles y stock disponibles</label>
-              <div class="form-row">
-                <div class="col-center flex">
-                  <div class="form-check-inline nt-2 me-3">
-                    <input class="form-check-input" type="checkbox" value="Talle 1" name="sizes" id="talle1">
-                    <label class="form-check-label" for="talle1">Talle 1</label>
-                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle1>
-                  </div>
-                  <div class="form-check-inline me-3">
-                    <input class="form-check-input" type="checkbox" value="Talle 2" name="sizes" id="talle2">
-                    <label class="form-check-label" for="talle2">Talle 2</label>
-                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle2>
-                  </div>
-                  <div class="form-check-inline me-3">
-                    <input class="form-check-input" type="checkbox" value="Talle 3" name="sizes" id="talle3">
-                    <label class="form-check-label" for="talle3">Talle 3</label>
-                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle3>
-                  </div>
-                  <div class="form-check-inline me-3">
-                    <input class="form-check-input" type="checkbox" value="Talle 4" name="sizes" id="talle4">
-                    <label class="form-check-label" for="talle4">Talle 4</label>
-                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle4>
-                  </div>
-                  <div class="form-check-inline me-3">
-                    <input class="form-check-input" type="checkbox" value="Talle 5" name="sizes" id="talle5">
-                    <label class="form-check-label" for="talle5">Talle 5</label>
-                    <input type="number" min="0" class="form-control mt-2" placeholder="Stock" data-stock-talle5>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="form-group general-stock-container d-none"> <!-- Oculto por defecto -->
-  <label for="generalStock">Stock general</label>
-  <input type="number" min="0" class="form-control mt-2" placeholder="Stock" id="generalStock" name="generalStock">
-</div>
-
-
-            <div class="form-group">
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="isFeatured" name="isFeatured">
-                <label class="form-check-label" for="isFeatured">Destacar producto</label>
-              </div>
-            </div>
-
-            <button type="submit" class="btn btn-primary btn-lg">Agregar</button>
-          </form>
+        <div class="mb-3">
+          <label>Precio</label>
+          <input class="form-control p-2" type="number" step="0.01" name="price" data-price required />
         </div>
-      </div>
-    </div>
-  `;
+        <div class="mb-3">
+          <label>Descripción</label>
+          <textarea class="form-control p-2" name="description" data-description required></textarea>
+        </div>
+        <div class="mb-3">
+          <label>Imágenes</label>
+          <input class="form-control p-2" type="file" name="images" data-imageUrls multiple required autofocus accept="image/*" max="2" />
+        </div>
+        <div class="mb-3">
+          <label>Sección</label>
+          <select id="miMenuDesplegable" class="form-control p-2">
+            <option value="opcion1">Opción 1</option>
+            <option value="opcion2">Opción 2</option>
+            <option value="opcion3">Opción 3</option>
+          </select>
+        </div>
+        <div id="sizesSection" class="mb-3">
+          <label>Selecciona los talles disponibles:</label>
+          <div>
+            <label><input type="checkbox" name="sizes" value="XS"> XS</label>
+            <input type="number" placeholder="Stock XS" data-stock-xs style="display: none;" />
+            <label><input type="checkbox" name="sizes" value="S"> S</label>
+            <input type="number" placeholder="Stock S" data-stock-s style="display: none;" />
+            <label><input type="checkbox" name="sizes" value="M"> M</label>
+            <input type="number" placeholder="Stock M" data-stock-m style="display: none;" />
+            <label><input type="checkbox" name="sizes" value="L"> L</label>
+            <input type="number" placeholder="Stock L" data-stock-l style="display: none;" />
+            <label><input type="checkbox" name="sizes" value="XL"> XL</label>
+            <input type="number" placeholder="Stock XL" data-stock-xl style="display: none;" />
+          </div>
+        </div>
+        <div id="generalStockSection" class="mb-3" style="display: none;">
+          <label>Stock General</label>
+          <input class="form-control p-2" type="number" id="generalStock" name="generalStock" />
+        </div>
+        <div class="mb-3">
+          <label><input type="checkbox" id="isFeatured" /> Producto destacado</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Crear Producto</button>
+      </form>
+    `;
     return card;
   }
 
-  // Configurar el evento change en el select
   setupSelectChangeHandler() {
-    const sectionSelect = this.titulo.querySelector("#miMenuDesplegable");
-    sectionSelect.addEventListener("change", () =>
-      this.updateSizesVisibility()
-    );
-  }
-
-  // Lógica para actualizar la visibilidad de los talles
-  updateSizesVisibility() {
-    const sectionSelect = document.getElementById("miMenuDesplegable");
-    const sizesContainer = document.querySelector(".sizes-container");
-    const generalStockContainer = document.querySelector(
-      ".general-stock-container"
-    );
-    const selectedValue = sectionSelect.value;
-
-    if (selectedValue === "opcion3") {
-      // Si es "Diversos", oculta los talles y muestra el campo de stock general
-      sizesContainer.classList.add("d-none");
-      generalStockContainer.classList.remove("d-none");
-    } else {
-      // Si no es "Diversos", muestra los talles y oculta el stock general
-      sizesContainer.classList.remove("d-none");
-      generalStockContainer.classList.add("d-none");
+    const sectionSelect = this.sectionSelect;
+    if (!this.sectionSelectHandler) {
+      this.sectionSelectHandler = () => this.updateSizesVisibility();
+      sectionSelect.addEventListener("change", this.sectionSelectHandler);
     }
   }
 
-  // Capturar el evento submit
+  updateSizesVisibility() {
+    const selectedValue = this.sectionSelect.value;
+    const sizesSection = document.getElementById("sizesSection");
+    const generalStockSection = document.getElementById("generalStockSection");
+
+    if (selectedValue === "opcion3") {
+      sizesSection.style.display = "none";
+      generalStockSection.style.display = "block";
+    } else {
+      sizesSection.style.display = "block";
+      generalStockSection.style.display = "none";
+    }
+  }
+
+  collectSizesAndStock() {
+    const selectedSizes = Array.from(
+      document.querySelectorAll('input[name="sizes"]:checked')
+    ).map((checkbox) => {
+      const size = checkbox.value;
+      const stockInput = document.querySelector(
+        `[data-stock-${size.toLowerCase().replace(" ", "")}]`
+      );
+      return { size, stock: stockInput ? parseInt(stockInput.value) : 0 };
+    });
+    return selectedSizes;
+  }
+
   setupFormSubmitHandler() {
-    const form = this.titulo.querySelector("[data-form]");
-    form.addEventListener("submit", (e) => {
-      const images = document.querySelector("[data-imageUrls]").files;
-
-      // Validar que solo se puedan seleccionar 2 imágenes
-      if (images.length > 2) {
-        e.preventDefault(); // Evitar el envío del formulario
-        alert("Por favor, selecciona un máximo de 2 imágenes."); // Mensaje de alerta
-        return; // Salir de la función
-      }
-
+    const form = this.titulo.querySelector("form");
+    form.addEventListener("submit", async (e) => {
       e.preventDefault();
-      this.handleSubmit();
+      await this.handleSubmit();
     });
   }
 
   async handleSubmit() {
-    const name = document.querySelector("[data-name]").value;
-    const price = parseFloat(document.querySelector("[data-price]").value);
-    const description = document.querySelector("[data-description]").value;
-    const section = document.getElementById("miMenuDesplegable").value;
+    const productData = new FormData();
+    productData.append("name", document.querySelector("[data-name]").value);
+    productData.append(
+      "price",
+      parseFloat(document.querySelector("[data-price]").value)
+    );
+    productData.append(
+      "description",
+      document.querySelector("[data-description]").value
+    );
+    productData.append("section", this.sectionSelect.value);
+    productData.append(
+      "isFeatured",
+      document.getElementById("isFeatured").checked
+    );
+
+    // Agregar imágenes
     const images = document.querySelector("[data-imageUrls]").files;
-    const isFeatured = document.getElementById("isFeatured").checked;
-
-    let productData = new FormData();
-    productData.append("name", name);
-    productData.append("price", price);
-    productData.append("description", description);
-    productData.append("section", section);
-    productData.append("isFeatured", isFeatured);
-
-    // Agrega cada archivo de imagen al FormData
     for (const image of images) {
       productData.append("images[]", image);
     }
 
-    // Capturar datos de stock dependiendo de la sección seleccionada
-    if (section === "opcion3") {
-      // Si es "Diversos", capturar el stock general
-      const generalStock =
-        parseInt(document.getElementById("generalStock").value) || 0;
-      productData.append("generalStock", generalStock);
+    // Manejo del stock según la sección
+    if (this.sectionSelect.value === "opcion3") {
+      productData.append(
+        "generalStock",
+        parseInt(document.getElementById("generalStock").value) || 0
+      );
     } else {
-      // Si no es "Diversos", capturar los talles y sus stocks
-      const selectedSizes = Array.from(
-        document.querySelectorAll('input[name="sizes"]:checked')
-      ).map((checkbox) => {
-        const size = checkbox.value;
-        const stockInput = document.querySelector(
-          `[data-stock-${size.toLowerCase().replace(" ", "")}]`
-        );
-        const stock = stockInput ? parseInt(stockInput.value) : 0;
-        return { size, stock };
-      });
-
-      productData.append("sizes", JSON.stringify(selectedSizes));
+      const sizesStock = this.collectSizesAndStock();
+      productData.append("sizes", JSON.stringify(sizesStock));
     }
 
     try {
