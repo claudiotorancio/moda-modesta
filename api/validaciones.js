2c7923223df615f2bb567f101646c203acdde6fb
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-10-24 22:59:25 -0300

    agrego express-validator a routes.js 3

--- api/validaciones.js
+++ api/validaciones.js
@@ -365,7 +365,6 @@ export {
   validacionesInfoPersonal,
   validacionesPedidosRecientes,
   validacionesNotificacionesSinStock,
-  validacionesGetNotificaciones,
   validacionesNotificacionIngreso,
   validacionesAgregarResena,
   validacionesPutResena,
