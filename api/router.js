35aaf20a8900a8d9260ac9d9ae2531f00ab8fd0b
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-10-24 22:53:06 -0300

    agrego express-validator a routes.js 1

--- api/router.js
+++ api/router.js
@@ -100,6 +100,7 @@ import confirmResetpassword from "../backend/routes/login/confirmResetPassword.j
 import { profileControllers } from "../backend/profile/profileControllers.js";
 import { isAuthenticated } from "../backend/isAuthenticated.js";
 import { validationResult } from "express-validator";
+import { kMaxLength } from "buffer";
 
 const router = Router();
 const app = express();
@@ -240,10 +241,15 @@ router.get(
 );
 
 // Notificaciones
-router.post("/api/notificacionSinStock", notificacionSinStock);
+router.post(
+  "/api/notificacionSinStock",
+  validacionesNotificacionesSinStock,
+  handleValidationErrors,
+  notificacionSinStock
+);
 router.get(
   "/api/getNotificaciones",
-  validacionesNotificacionesSinStock,
+  validacionesGetNotificaciones,
   handleValidationErrors,
   authenticateJWT,
   requireAdmin,
