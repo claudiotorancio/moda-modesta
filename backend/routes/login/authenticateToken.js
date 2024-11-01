cef1b795c3147d3d11492a3ad44f5832635c9fef
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-11-01 01:57:43 -0300

    arreglo logica para trabajar en desarrollo como prod 1

--- backend/routes/login/authenticateToken.js
+++ backend/routes/login/authenticateToken.js
@@ -1,18 +1,27 @@
-//authenticateToken
+//
 
 import jwt from "jsonwebtoken";
 
+// Middleware de autenticación
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  // Solo aplicar lógica de token en desarrollo
+  if (process.env.NODE_ENV === "development") {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) return res.status(401).json({ error: "Token no proporcionado" });
+    if (!token)
+      return res.status(401).json({ error: "Token no proporcionado" });
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ error: "Token inválido" });
-    req.user = user; // Añadir el usuario al request
-    next();
-  });
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) return res.status(403).json({ error: "Token inválido" });
+      req.user = user; // Añadir el usuario al request
+      next();
+    });
+  } else {
+    // En producción, usar sesión de Passport
+
+    return next(); // El usuario está autenticado, continúa
+  }
 };
 
 export default authenticateToken;
