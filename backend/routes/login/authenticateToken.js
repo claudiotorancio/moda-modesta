7e34641c204be9963e3a25a4a1d4a6dbe7f878e3
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-11-01 04:55:23 -0300

    arreglo logica para trabajar en desarrollo como prod 6

--- backend/routes/login/authenticateToken.js
+++ backend/routes/login/authenticateToken.js
@@ -2,7 +2,9 @@ import jwt from "jsonwebtoken";
 
 // Middleware de autenticación
 const authenticateToken = (req, res, next) => {
-  const publicRoutes = ["/api/listaProductos"]; // Rutas públicas
+  // Lista de rutas públicas que no requieren autenticación
+
+  const publicRoutes = ["/api/listaProductos"]; // Agrega más rutas públicas si es necesario
 
   // Verificar si la ruta es pública
   if (publicRoutes.includes(req.path)) {
@@ -10,40 +12,39 @@ const authenticateToken = (req, res, next) => {
   }
 
   // Solo aplicar lógica de token en desarrollo
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  if (process.env.NODE_ENV === "development") {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) {
-    return res.status(401).json({ error: "Token no proporcionado" });
-  }
-
-  // Verificar el token
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ error: "Token inválido" });
+    if (!token) {
+      return res.status(401).json({ error: "Token no proporcionado" });
     }
-    const { user } = decoded;
-    const { _id, email, role, nombre } = user;
 
-    req.user = {
-      _id,
-      email,
-      role,
-      nombre,
-    };
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(403).json({ error: "Token inválido" });
+      }
+      const { user } = decoded; // Obtén el objeto user
+      const { _id, email, role, nombre } = user; // Desestructura los valores de user
 
-    // En producción, verificar si el usuario es un administrador
-    if (
-      process.env.NODE_ENV !== "development" &&
-      (!req.isAuthenticated() || req.user.role !== "admin")
-    ) {
+      req.user = {
+        _id,
+        email,
+        role,
+        nombre,
+      };
+
+      next();
+    });
+  } else {
+    // En producción, usar sesión de Passport
+    if (!req.isAuthenticated() || req.user.role !== "admin") {
       return res.status(403).json({
         error: "Acceso denegado: Solo usuarios administradores pueden acceder",
       });
     }
-
     next();
-  });
+  }
 };
 
 export default authenticateToken;
