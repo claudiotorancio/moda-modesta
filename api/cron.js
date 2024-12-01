e1cad1127473c261e0254abf6bd804b83f392aa8
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-12-01 14:16:00 -0300

    corrijo trabajso cron 1

--- api/cron.js
+++ api/cron.js
@@ -1,43 +1,24 @@
-// api/cron.
 import moment from "moment";
 import Vista from "../backend/models/Vista.js";
-import mongoose from "mongoose";
-import MONGODB_URI from "../backend/config.js";
+import { connectToDatabase } from "../backend/utils/db.js"; // Asumiendo que el caché está aquí
 
 export default async function handler(req, res) {
-  // if (req.method === "GET") {
-  // Conectar a la base de datos y guardar el producto
-  await mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
   try {
-    const currentDate = new Date();
+    // Conexión a la base de datos usando caché
+    await connectToDatabase();
 
-    const parsedDate = moment.utc(currentDate);
+    const currentDate = moment.utc().toDate(); // Formato UTC compatible con MongoDB
 
-    // Buscar productos cuyo discountExpiry haya pasado
-    const productosConDescuentoExpirado = await Vista.find({
-      discountExpiry: { $lte: parsedDate },
-    });
-    // Actualizar los productos con descuento expirado
-    await Promise.all(
-      productosConDescuentoExpirado.map(async (producto) => {
-        producto.discount = 0;
-        producto.discountExpiry = ""; // Opcional: Puedes eliminar la fecha de expiración
-        await producto.save();
-      })
+    // Buscar y actualizar productos en lote
+    const result = await Vista.updateMany(
+      { discountExpiry: { $lte: currentDate } },
+      { $set: { discount: 0, discountExpiry: "" } }
     );
 
+    console.log(`${result.nModified} productos actualizados`);
     res.status(200).json({ message: "Productos actualizados exitosamente" });
   } catch (error) {
-    console.error(
-      "Error al actualizar productos con descuento expirado:",
-      error
-    );
+    console.error("Error al actualizar productos:", error);
     res.status(500).send("Error al ejecutar cron job");
   }
-  // } else {
-  //   res.status(405).send("Método no permitido");
-  // }
 }
