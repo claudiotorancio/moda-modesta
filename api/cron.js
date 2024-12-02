0430beb9eb4438e5a682e856899ad6647305ada5
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-12-02 00:29:04 -0300

    corrijo fcion cron 4

--- api/cron.js
+++ api/cron.js
@@ -1,3 +1,4 @@
+import moment from "moment";
 import Vista from "../backend/models/Vista.js";
 import { connectToDatabase } from "../backend/db/connectToDatabase.js";
 
@@ -7,7 +8,7 @@ export default async function handler(req, res) {
     try {
       // Conexión a la base de datos usando caché
 
-      const currentDate = new Date(); // Formato UTC compatible con MongoDB
+      const currentDate = moment.utc().toDate(); // Formato UTC compatible con MongoDB
       console.log(currentDate);
       // Buscar y actualizar productos en lote
       const result = await Vista.updateMany(
