9a29e39064c6a3dae7005309266218b4f66d0bcf
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-12-02 01:29:49 -0300

    corrijo fcion cron 11

--- api/cron.js
+++ api/cron.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     try {
       // Conexión a la base de datos usando caché
 
-      const currentDate = moment(new Date()); // Formato UTC compatible con MongoDB
+      const currentDate = moment.utc(new Date()); // Formato UTC compatible con MongoDB
       console.log(currentDate);
       // Buscar y actualizar productos en lote
       const result = await Vista.updateMany(
