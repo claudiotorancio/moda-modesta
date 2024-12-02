7716e5336c06acd105e23efb1c97b5112deaf319
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-12-02 00:16:37 -0300

    corrijo fcion cron 2

--- api/cron.js
+++ api/cron.js
@@ -1,7 +1,6 @@
 import moment from "moment";
 import Vista from "../backend/models/Vista.js";
 import { connectToDatabase } from "../backend/db/connectToDatabase.js";
-import { toDate } from "validator";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
@@ -9,11 +8,14 @@ export default async function handler(req, res) {
     try {
       // Conexión a la base de datos usando caché
 
-      const currentDate = moment.utc().toDate(); // Formato UTC compatible con MongoDB
-      console.log(currentDate);
+      const currentDate = new Date();
+
+      const parsedDate = moment.utc(currentDate);
+
+      console.log(parsedDate);
       // Buscar y actualizar productos en lote
       const result = await Vista.updateMany(
-        { discountExpiry: { $lte: currentDate } },
+        { discountExpiry: { $lte: parsedDate } },
         { $set: { discount: 0, discountExpiry: "" } }
       );
 
