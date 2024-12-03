add942b161d2cd3c6ea3236dfda7664b185cd2e9
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2024-12-02 23:08:06 -0300

    corrijo creacion de sessionId en carrito 20

--- frontend/controllers/carrito/carrito.js
+++ frontend/controllers/carrito/carrito.js
@@ -21,18 +21,9 @@ class Carrito {
     this.items = cargarCarritoDesdeStorage.call(this) || [];
   }
 
-  generateSessionId() {
-    // Generar un sessionId único (puedes usar cualquier método de generación de ID único)
-    return "session_" + Math.random().toString(36).substr(2, 9);
-  }
-
   // Recuperar o generar sessionId
   obtenerOGenerarSessionId() {
     let sessionId = localStorage.getItem("sessionId");
-    if (!sessionId) {
-      sessionId = this.generateSessionId();
-      localStorage.setItem("sessionId", sessionId);
-    }
     return sessionId;
   }
 
