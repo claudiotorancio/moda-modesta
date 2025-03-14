46ef9f0145d3363cd0102e4e41ea8f3d8d478d96
Claudio <101899480+claudiotorancio@users.noreply.github.com>
2025-03-14 01:25:29 -0300

    correccion al agregar producto al carrito 5

--- frontend/controllers/productos/agregarProductoCarrito.js
+++ frontend/controllers/productos/agregarProductoCarrito.js
@@ -73,6 +73,9 @@ export async function agregarProductoCarrito() {
         await carrito.agregarProducto(producto);
       } catch (error) {
         console.error("Error al agregar el producto al carrito:", error);
+      } finally {
+        // Rehabilitar el botón después de que la operación haya terminado
+        carritoButton.disabled = false;
       }
     });
   }
